6e20ea4ced0789db908f908cf6a19a707af0fcd9
jianqi-hu <hulab-photonics@tutamail.com>
2025-10-19 17:33:57 +0800

    CI fix: remove unused Link import (Home.tsx) and socialLinks var (Footer.tsx)

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 
 import integratedOptics from '../assets/integrated-optics.jpg';
 import nonlinearPhotonics from '../assets/nonlinear-photonics.jpg';
